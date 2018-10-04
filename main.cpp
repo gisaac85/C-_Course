954ce71c2be0a40ba4de3d5e162748783b38a8cb
Gaorieh Isaac <gaorieh@hotmail.com>
2018-10-04 21:41:34 +0200

    fsdfsdf

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello World!\n";
+  string fName,lName;
+  int age;
+  cout<<"enter first name:";
+  cin>>fName;
+  cout<<"enter last name:";
+  cin>>lName;
+  cout<<"enter age:";
+  cin>>age;
+  cout<<"Your Full name is: "<<fName<<" "<<lName;
+  cout<<"\nYour Age is: "<<age;
+  system("pause");
+  //end
+}
