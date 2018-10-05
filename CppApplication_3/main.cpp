8668f313ec0c182aa087749c9ff04e7ff16c2cff
Gaorieh Isaac <gaorieh@hotmail.com>
2018-10-05 18:06:01 +0200

    get&set ver1

--- CppApplication_3/main.cpp
+++ CppApplication_3/main.cpp
@@ -29,10 +29,19 @@ private:
 int main() {
     
     Cars myCar;
+    
+    string cModel;
+    int mSpeed;
+    
+    cout << "Enter Your Car Model: ";
+    cin >>cModel;
+    myCar.setCarModel(cModel);
    
-    myCar.setCarModel("Mazda");
-    myCar.setCarMaxSpeed(220);
-    cout << myCar.getCarModel();
-    cout << myCar.getCarMaxSpeed();
+    cout << "Enter Car Max Speed: ";
+    cin >>mSpeed;
+    myCar.setCarMaxSpeed(mSpeed);
+    
+    cout << "Your Car Model is: "<<myCar.getCarModel();
+    cout << " And Its max Speed= "<< myCar.getCarMaxSpeed();
     return 0; 
 }
